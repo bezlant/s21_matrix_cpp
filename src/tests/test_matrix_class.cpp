a344621ad13684bf98fa91da651f7882b9083880
bezlant <abezludniy@mail.ru>
2022-08-25 09:47:29 +0300

    Tested the overloaded operators

--- src/tests/test_matrix_class.cpp
+++ src/tests/test_matrix_class.cpp
@@ -52,3 +52,188 @@ TEST(test_class, square_brackets_operator) {
     m[5][8] = 69.420;
     EXPECT_EQ(m[5][8], 69.420);
 }
+
+TEST(test_setters, set_rows_up) {
+    S21Matrix m(2, 2);
+    m[1][1] = 6.9;
+
+    m.set_rows(5);
+    EXPECT_EQ(m.get_rows(), 5);
+    EXPECT_EQ(m[1][1], 6.9);
+}
+
+TEST(test_setters, set_rows_down) {
+    S21Matrix m(2, 2);
+    m[0][0] = 6.9;
+
+    m.set_rows(1);
+    EXPECT_EQ(m.get_rows(), 1);
+    EXPECT_EQ(m[0][0], 6.9);
+}
+
+TEST(test_setters, set_rows_zero) {
+    S21Matrix m;
+
+    EXPECT_ANY_THROW(m.set_rows(0));
+}
+
+TEST(test_setters, set_cols_up) {
+    S21Matrix m(2, 2);
+    m[1][1] = 6.9;
+
+    m.set_cols(5);
+    EXPECT_EQ(m.get_cols(), 5);
+    EXPECT_EQ(m[1][1], 6.9);
+}
+
+TEST(test_setters, set_cols_down) {
+    S21Matrix m(2, 2);
+    m[0][0] = 6.9;
+
+    m.set_cols(1);
+    EXPECT_EQ(m.get_cols(), 1);
+    EXPECT_EQ(m[0][0], 6.9);
+}
+
+TEST(test_setters, set_cols_zero) {
+    S21Matrix m;
+
+    EXPECT_ANY_THROW(m.set_cols(0));
+}
+
+TEST(test_overload, equal_lvalue) {
+    S21Matrix m;
+    S21Matrix x(3, 6);
+    x(1, 2) = 3;
+    x(2, 2) = 3;
+
+    m = x;
+
+    EXPECT_EQ(m.get_rows(), 3);
+    EXPECT_EQ(m.get_cols(), 6);
+    EXPECT_EQ(m(1, 2), 3);
+    EXPECT_EQ(m(2, 2), 3);
+}
+
+TEST(test_overload, equal_rvalue) {
+    S21Matrix m(2, 2);
+    S21Matrix n;
+
+    n = std::move(m);
+
+    EXPECT_EQ(n.get_cols(), 2);
+    EXPECT_EQ(n.get_rows(), 2);
+}
+
+TEST(test_functional, eq_matrix) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    ASSERT_TRUE(m == n);
+}
+
+TEST(test_functional, eq_matrix1) {
+    S21Matrix m(3, 2);
+    S21Matrix n;
+
+    m[0][1] = 69;
+    n = m;
+
+    ASSERT_TRUE(m == n);
+}
+
+TEST(test_functional, sum_matrix) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    m(0, 0) = 1;
+    n(0, 0) = 1;
+
+    m(1, 1) = 1;
+    n(1, 1) = 1;
+
+    m.SumMatrix(n);
+
+    EXPECT_EQ(m(0, 0), 2);
+    EXPECT_EQ(m(1, 1), 2);
+}
+
+TEST(test_functional, add_operator_overload) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    m(0, 0) = 1;
+    n(0, 0) = 1;
+
+    m(1, 1) = 1;
+    n(1, 1) = 1;
+
+    m = m + n;
+
+    EXPECT_EQ(m(0, 0), 2);
+    EXPECT_EQ(m(1, 1), 2);
+}
+
+TEST(test_functional, eqadd_operator_overload) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    m(0, 0) = 1;
+    n(0, 0) = 1;
+
+    m(1, 1) = 1;
+    n(1, 1) = 1;
+
+    m += m + n;
+
+    EXPECT_EQ(m(0, 0), 3);
+    EXPECT_EQ(m(1, 1), 3);
+}
+
+TEST(test_functional, sub_matrix) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    m(0, 0) = 1;
+    n(0, 0) = 1;
+
+    m(1, 1) = 1;
+    n(1, 1) = 1;
+
+    m.SubMatrix(n);
+
+    EXPECT_EQ(m(0, 0), 0);
+    EXPECT_EQ(m(1, 1), 0);
+}
+
+TEST(test_functional, eqsub_overloaded_operator) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    m(0, 0) = 1;
+    n(0, 0) = 1;
+
+    m(1, 1) = 1;
+    n(1, 1) = 1;
+
+    m -= n;
+
+    EXPECT_EQ(m(0, 0), 0);
+    EXPECT_EQ(m(1, 1), 0);
+}
+
+TEST(test_functional, sub_operator_overload) {
+    S21Matrix m(2, 2);
+    S21Matrix n(2, 2);
+
+    m(0, 0) = 1;
+    n(0, 0) = 1;
+
+    m(1, 1) = 1;
+    n(1, 1) = 1;
+
+    m = m - n;
+
+    EXPECT_EQ(m(0, 0), 0);
+    EXPECT_EQ(m(1, 1), 0);
+}
